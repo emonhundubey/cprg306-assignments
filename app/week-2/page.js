039b6d84a919805539cf254208cb8bd4cbab8749
Emon <123427775+emonhundubey@users.noreply.github.com>
2024-01-18 11:55:28 -0700

    Added content to week 2
    name and github address added

---
+++ app/week-2/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+      <main>
+        <h1>Shopping List</h1>
+        <h1>Emon Hundubey</h1>
+        <h1>https://github.com/emonhundubey</h1>
+      </main>
+    );
+  }
