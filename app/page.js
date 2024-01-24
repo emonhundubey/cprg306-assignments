45f56df3a22327d6e46e391df9b34a9b55b57b43
Emon <123427775+emonhundubey@users.noreply.github.com>
2024-01-24 13:16:43 -0700

    update page
    minor change

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Page() {
     return (
       <main  className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1 className="text-4xl">CPRG 306: Web Development 2 - Assignments</h1>
-          <h2 className="text-2xl">Week 2</h2>
+          <h2 className="text-2xl">Week-2</h2>
           <div href= "http://localhost:3000/week-2"></div>
       </main>
     );
