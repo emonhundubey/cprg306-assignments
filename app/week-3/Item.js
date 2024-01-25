50bce408a6a0c96d21ed454b327ebe5f670acfe9
Emon <123427775+emonhundubey@users.noreply.github.com>
2024-01-25 11:59:01 -0700

    tailwind changes
    making adjustments with tailwind

--- app/week-3/Item.js
+++ app/week-3/Item.js
@@ -1,9 +1,9 @@
 const Item = ({ name, quantity, category }) => {
   return (
-    <div className="">
-      <div>{name}</div>
-      <div>
-        buy {quantity} in {category}
+    <div className="bg-slate-900 p-2 m-4 max-w-sm">
+      <div  className="font-bold text-xl">{name}</div>
+      <div className="pb-2 text-sm">
+        Buy {quantity} in {category}
       </div>
     </div>
   );
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -5,7 +5,7 @@ const Page = (props) => {
  
     return (
       <main>
-        <h1>Shopping List</h1>
+        <h1 className="font-bold text-3xl m-2">Shopping List</h1>
         <ItemList />
       </main>
     );  
