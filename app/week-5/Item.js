03c4b38ba5c906889c0a6e36bd8f98a269ad8390
Emon <123427775+emonhundubey@users.noreply.github.com>
2024-02-27 18:44:44 -0700

    small changes
    adding week 6

--- app/week-5/Item.js
+++ app/week-5/Item.js
@@ -1,7 +1,8 @@
+
 const Item = ({ name, quantity, category }) => {
   return (
     <div className="bg-slate-900 p-2 m-4 max-w-sm text-white">
-      <div  className="font-bold text-xl">{name}</div>
+      <div className="font-bold text-xl">{name}</div>
       <div className="text-sm">
         Buy {quantity} in {category}
       </div>
