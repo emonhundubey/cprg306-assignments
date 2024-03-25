1ece9529118e3b649a84b9ec306154003c9628a5
Emon <123427775+emonhundubey@users.noreply.github.com>
2024-03-25 16:52:13 -0600

    update

---
+++ app/week-10/_services/shopping-list-service.js
@@ -0,0 +1,32 @@
+import { db } from "../_utils/firebase";
+import { collection, getDocs, addDoc, query } from "firebase/firestore";
+
+export const addOneItems = async (userId, item) => {
+    try {
+      const updateItem = { ...item, quantity: +item.quantity };
+      const newItem = await addDoc(
+        collection(db, "users", userId, "items"),
+        updateItem
+      );
+      return newItem.id;
+    } catch (error) {
+      console.error("Error adding item:", error);
+      throw error;
+    }
+  };
+  
+  export const getOneItem = async (userId, itemId) => {
+    try {
+      const docRef = doc(db, "users", userId, "items", itemId);
+      const docSnap = await getDoc(docRef);
+  
+      if (docSnap.exists()) {
+        return { id: docSnap.id, data: docSnap.data() };
+      } else {
+        return null; 
+      }
+    } catch (error) {
+      console.error("Error getting item:", error);
+      throw error;
+    }
+  };
